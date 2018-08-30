fa7663b9bc4b38ff137b9c74447e84d494677b4e
Atish Patra <atish.patra@wdc.com>
2018-08-29 17:59:05 -0700

    Revert "Revert "phylib: Add device reset GPIO support""
    This reverts commit 6b5116201c4efc4529fc41d245a78bbaecc8eb1f.

--- drivers/net/phy/mdio_bus.c
+++ drivers/net/phy/mdio_bus.c
@@ -72,16 +72,8 @@ static int mdiobus_register_gpiod(struct mdio_device *mdiodev)
 
 int mdiobus_register_device(struct mdio_device *mdiodev)
 {
-	int err;
-
 	if (mdiodev->bus->mdio_map[mdiodev->addr])
 		return -EBUSY;
-
-	if (mdiodev->flags & MDIO_DEVICE_FLAG_PHY) {
-		err = mdiobus_register_gpiod(mdiodev);
-		if (err)
-			return err;
-	}
 
 	mdiodev->bus->mdio_map[mdiodev->addr] = mdiodev;
 
